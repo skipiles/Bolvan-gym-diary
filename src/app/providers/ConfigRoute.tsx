f660191e583647d01b3f8b7534745f72ab64d148
Mister Masya <skipiles@gmail.com>
2025-10-10 21:02:44 +0800

    feat: add colored navigation and home profile page

--- src/app/providers/ConfigRoute.tsx
+++ src/app/providers/ConfigRoute.tsx
@@ -5,12 +5,18 @@ import Workout from '@pages/Workout/Workout'
 import type { ReactNode } from 'react'
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
 import WaterDropIcon from '@mui/icons-material/WaterDrop'
+import { Home } from '@/pages/Home'
+import HomeIcon from '@mui/icons-material/Home'
 
 export const routesConfig: RouteObject[] = [
   {
     path: '/',
     element: <Layout />,
     children: [
+      {
+        index: true,
+        element: <Home />,
+      },
       {
         path: '/water',
         element: <Water />,
@@ -28,9 +34,21 @@ export type NavigationItem = {
   title: string
   icon?: ReactNode
   disabled?: boolean
+  activeColor?: string
 }
 
 export const navigationItems: NavigationItem[] = [
-  { path: '/water', title: 'Water', icon: <WaterDropIcon /> },
-  { path: '/workout', title: 'Workout', icon: <FitnessCenterIcon /> },
+  { path: '/', title: 'Home', icon: <HomeIcon />, activeColor: '#cd853f' },
+  {
+    path: '/water',
+    title: 'Water',
+    icon: <WaterDropIcon />,
+    activeColor: '#007AFF',
+  },
+  {
+    path: '/workout',
+    title: 'Workout',
+    icon: <FitnessCenterIcon />,
+    activeColor: '#ff6666',
+  },
 ]
